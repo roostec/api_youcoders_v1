94420bff82df210b1827eb611b8417005ee75964
Rafael Araujo <rflaraujodev@gmail.com>
2022-03-22 18:19:11 -0300

    /auth

--- src/routes/accountRouter.js
+++ src/routes/accountRouter.js
@@ -5,7 +5,7 @@ const passport = require('../utils/passportSetup');
 
 router.get("/failed", (req, res) => res.send("Failed"))
 router.get("/success", (req, res) => res.status(200).redirect(`${process.env.CLIENT_URL}/main/token/${req.user.accessToken}`).end())
-router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get('/', passport.authenticate('google', { scope: ['profile'] }));
 router.get('/auth/callback', passport.authenticate('google', { failureRedirect: '/failed', successRedirect: '/success', }),);
 router.get("/logout", (req, res) => {
     req.logout();
