b521a4d4f01a1cedfdf3e2e81283f657043db61d
Rafael Araujo <rflaraujodev@gmail.com>
2022-03-22 18:08:59 -0300

    ajuste env passport

--- src/routes/accountRouter.js
+++ src/routes/accountRouter.js
@@ -4,7 +4,7 @@ const passport = require('../utils/passportSetup');
 
 
 router.get("/failed", (req, res) => res.send("Failed"))
-router.get("/success", (req, res) => res.status(200).redirect(`http://192.168.1.11:8080/main/token/${req.user.accessToken}`).end())
+router.get("/success", (req, res) => res.status(200).redirect(`${process.env.CLIENT_URL}/main/token/${req.user.accessToken}`).end())
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 router.get('/auth/callback', passport.authenticate('google', { failureRedirect: '/failed', successRedirect: '/success', }),);
 router.get("/logout", (req, res) => {
