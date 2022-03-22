be3dd2de4265b9a3ca66b885a3d1ee6baedf7a3a
Rafael Araujo <rflaraujodev@gmail.com>
2022-03-22 19:17:16 -0300

    get profile google

--- src/routes/accountRouter.js
+++ src/routes/accountRouter.js
@@ -4,7 +4,15 @@ const passport = require('../utils/passportSetup');
 
 
 router.get("/failed", (req, res) => res.send("Failed"))
-router.get("/success", (req, res) => res.status(200).redirect(`${process.env.CLIENT_URL}/main/token/${req.user.accessToken}`).end())
+router.get("/success", (req, res) => {
+    const { accessToken } = req.user
+    const profile = { ...req.user.profile._json }
+    profile.id = profile.sub
+    profile.picture = encodeURIComponent(profile.picture)
+    delete profile.sub
+    res.status(200).redirect(`${process.env.CLIENT_URL}/main/${JSON.stringify(profile)}/${accessToken}`)
+})
+
 router.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
 router.get('/auth/callback', passport.authenticate('google', { failureRedirect: '/failed', successRedirect: '/success', }),);
 router.get("/logout", (req, res) => {
